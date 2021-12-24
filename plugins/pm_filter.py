fd8d1edc0a72547871e9c516a9bd8e3f53c2227e
fuckingmywife <76033254+fuckingmywife@users.noreply.github.com>
2021-12-24 19:32:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -345,4 +345,4 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "pages":
             await query.answer()
     else:
-        await query.answer("കൌതുകും ലേശം കൂടുതൽ ആണല്ലേ👀",show_alert=True)
+        await query.answer("You are not subscribed my channel👀",show_alert=True)
